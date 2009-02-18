6e047fdf90d085172049a11c8b1fc8845cb6c969
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2009-02-18 13:27:23 +0100

    Separated into files

--- toca.rb
+++ toca.rb
@@ -1,11 +1,9 @@
-require 'rubygems'
 require 'sinatra'
-require 'sass/plugin'
 
 $mp3dir = ARGV[0] 
 
 def playlist_song(song)
-  haml :playlist_song, :locals => {:file => song}, :layout => false
+  haml :_playlist_song, :locals => {:file => song}, :layout => false
 end
 
 get '/' do
@@ -24,50 +22,7 @@ post '/' do
   puts files.join("\n")
 end
 
-
-
-__END__
-
-@@ index
-
-:javascript
-  function init(){
-    $('li.song').click(function(){
-      $.ajax({
-        url: '/playlist_song/' + $(this).html(),
-        success: function(html){
-          $('ul#songs').append(html)
-        }
-       })
-    })  
-  }
-
-  $(init)
-
-
-#mp3s
-  %ul
-    - @mp3s.each do |f|
-      %li.song= f
-
-#playlist
-  %ul#songs
-    = playlist_song("pepe")
-
-
-
-@@ layout
-%html
-  %head
-    %title Toca
-    %script{ :type => "text/javascript", :src => "http://jqueryjs.googlecode.com/files/jquery-1.3.1.js"}
-    %style{:type => "text/css"}
-      :sass
-        body
-          font: 0.9em sans-serif
-  %body
-    #main
-      = yield
-
-@@ playlist_song
-%li= file 
+get '/toca.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :"sass/toca" 
+end
